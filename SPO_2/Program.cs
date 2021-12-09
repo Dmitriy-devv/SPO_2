6e03590e8bede7cd4b7f4b2983f7b14e74b4d9d9
Dmitry <malinkin20001@gmail.com>
2021-12-09 19:33:14 +0300

    Добавил TextReader

--- SPO_2/Program.cs
+++ SPO_2/Program.cs
@@ -6,7 +6,20 @@ namespace SPO_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var path = @"C:\SPO\note.txt";
+            var textReader = new TextReader(path);
+            var code = textReader.Read();
+
+            //Этап 1 - Лексический анализатор
+
+
+            Console.ReadKey();
+
+        }
+
+        private static void Space()
+        {
+            Console.WriteLine("------------------------------------------");
         }
     }
 }
